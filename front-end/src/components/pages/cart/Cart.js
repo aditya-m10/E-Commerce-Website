205bf8791bb864ccba9090f896c325890b22b499
Aditya Maurya <76245095+aditya12data@users.noreply.github.com>
2023-01-06 15:10:15 +0530

    fix: Cart map and UI   , feat: totalprice

--- front-end/src/components/pages/cart/Cart.js
+++ front-end/src/components/pages/cart/Cart.js
@@ -1,54 +1,68 @@
 import { IoMdCloseCircle } from "react-icons/io";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { increment, decrement, clear } from "../../../features/cartSlice";
 import { cartTotalPriceSelector } from "../../../features/selectors";
 
-
 const Sidebar = ({ close }) => {
   const cart = useSelector((state) => state.cart);
-  console.log(cart)
+  console.log(cart);
   const dispatch = useDispatch();
   const totalPrice = useSelector(cartTotalPriceSelector);
 
-
   return (
     <div className="sidebar">
-      <div onClick={() => close()}>
+      <div className="cross" onClick={() => close()}>
         <IoMdCloseCircle color="#000" size={20} />
       </div>
       {cart.length > 0 ? (
-          <button className="clear"  onClick={() => {
+        <button
+          className="clear"
+          onClick={() => {
             dispatch(clear());
-          }}>clear cart</button>
-        ) : (
-          <h3>Hurry Now!! Fill Your cart</h3>
-        )}
-      
+          }}
+        >
+          clear cart
+        </button>
+      ) : (
+        <h3>Hurry Now!! Fill Your cart</h3>
+      )}
+
       {cart.map((item) => (
-        <div key={item.id}  className="cart-wrapper" >
-          <div  className="image-wrapper ">
-            <img src={item.cover} alt={item.title} ></img>
+        <div key={item.id} className="cart-wrapper">
+          <div className="image-wrapper ">
+            <img src={item.cover} alt={item.title}></img>
           </div>
           <div className="text-wrapper ">
             <span>{item.title}</span>
           </div>
           <div className="price-wrapper ">
-            <span> Rs. {item.quantity * item.price }</span>
+            <span> Rs. {item.quantity * item.price}</span>
           </div>
           <div className="inc-dec">
-          <button onClick={() => {
-                    dispatch(increment(item.id));
-                  } }>+</button>
-          {item.quantity}
-          <button disabled={item.quantity === 1} onClick={() => {
-                    dispatch(decrement(item.id));
-                  } }>-</button>
+            <button
+              onClick={() => {
+                dispatch(increment(item.id));
+              }}
+            >
+              +
+            </button>
+            {item.quantity}
+            <button
+              disabled={item.quantity === 1}
+              onClick={() => {
+                dispatch(decrement(item.id));
+              }}
+            >
+              -
+            </button>
           </div>
           <button className="remove">Remove</button>
-
         </div>
-))}
+      ))}
+      <div className="total">
       
+      {totalPrice > 0 && <><hr /> <h1 > <a>Total</a> Rs. {totalPrice}</h1></>}
+      </div>
     </div>
   );
 };
